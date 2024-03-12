e3f3748481d58aedca20365d0dcca8669d074582
ddoavoca <ddoavoca@gmail.com>
2024-03-12 13:28:15 +0900

    Add redirecting to cursor key

--- MyGameServer/gameserver01/Client.cpp
+++ MyGameServer/gameserver01/Client.cpp
@@ -5,9 +5,9 @@ using namespace std;
 
 const int SIZE = 8; // 체스판의 크기
 int board[SIZE][SIZE]; // 체스판 데이터
-int x = 0, y = 0; // 현재 Pawn 위치
+int x = 0, y = 0; // 처음 Pawn 위치
 
-// 체스판을 초기화하는 함수
+// 체스판 초기화하는 함수
 void initializeBoard() 
 {
     for(int i = 0; i < SIZE; ++i) {
@@ -36,18 +36,19 @@ void printBoard()
             switch (board[i][j])
             {
             case 1:
-                cout << "■ ";
+                cout << " ■ ";
                 break;
             case -1:
-                cout << "□ ";
+                cout << " □ ";
                 break;
             case 0:
-                cout << "P ";
+                cout << " ♣ ";
                 break;
-
+               
             }
             
         }
+        cout << endl;
         cout << endl;
     }
 }
@@ -96,8 +97,43 @@ int main() {
                 }
                 break;
 
+             // 커서 키로 동작하도록
+            case 72: // 위쪽 화살표
+                if (y > 0) {
+                    // 이전 위치를 원래 바둑판 색으로 복원
+                    board[y][x] = ((y + x) % 2 == 0) ? -1 : 1;
+                    y--;
+
+                    board[y][x] = 0; // 새 위치에 Pawn 설정
+                }
+                break;
+            case 80: // 아래쪽 화살표
+                if (y < SIZE - 1) {
+                    board[y][x] = ((y + x) % 2 == 0) ? -1 : 1;
+                    y++;
+
+                    board[y][x] = 0;
+                }
+                break;
+            case 75: // 왼쪽 화살표
+                if (x > 0) {
+                    board[y][x] = ((y + x) % 2 == 0) ? -1 : 1;
+                    x--;
+
+                    board[y][x] = 0;
+                }
+                break;
+            case 77: // 오른쪽 화살표
+                if (x < SIZE - 1) {
+                    board[y][x] = ((y + x) % 2 == 0) ? -1 : 1;
+                    x++;
+
+                    board[y][x] = 0;
+                }
+                break;
+
         }
-        printBoard(); // 매 이동 후 체스판을 다시 그림
+        printBoard(); // 이동 후 체스판 다시 그림
     }
 
     return 0;
